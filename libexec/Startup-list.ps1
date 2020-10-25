ea7b16783440e8c1025019d775feca1912bfc7c9
IRONHIDE <optimus6699prime@gmail.com>
2020-10-25 21:32:37 +0800

    Improve Startup-list

--- libexec/Startup-list.ps1
+++ libexec/Startup-list.ps1
@@ -1,1 +1,3 @@
-ls "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+Write-Host "Directory:$env:userproile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+Write-Host "========================================================================"
+ls "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"|Foreach-Object {$_.name}
